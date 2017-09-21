b68d64fa3ca3494c9269bc8126826b66324c2248
haku-kiro <botPrime@yahoo.com>
2017-09-21 20:34:46 +0200

    adding backend and cleaning up front

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 console.log("test");
+var quote;
+var author;
+
 
 $(document).ready(function(){
-    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en#", function(json){
-        console.log(json);
+
+    //first quote has to be loaded on startup
+    getQuote();
+
+    //clicking on the new quote button
+    $("#newQuote").on("click", function()
+    {
+        getQuote();
+        //maybe change the text to 'another!' - rem to make sure that on load it is set to new quote though
     });
-});
+
+    $("#tweet").on("click", function()
+    {
+        var tweetString = '"' + quote+ '"' + " - " + author;
+        window.open("http://twitter.com/home/?status=" + tweetString);
+    });
+
+    function getQuote()
+    {
+        $.get("https://talaikis.com/api/quotes/random/", function(data){
+            quote = data.quote;
+            author = data.author;
+            $("#quoteSpace").html('"' + quote+ '"');
+            $("#author").html(" - " + author);
+        });
+    }
+
+
+ });   
